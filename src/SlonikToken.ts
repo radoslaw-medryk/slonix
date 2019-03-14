cf3725589cc8437f7e1d6d6baaa062658811f994
Radoslaw Medryk <radoslaw.medryk@yhoo.com>
2019-03-14 19:29:59 +0100

    Fix inproper handling slonik tokens

---
+++ src/SlonikToken.ts
@@ -0,0 +1,22 @@
+import {
+    IdentifierTokenType,
+    RawSqlTokenType,
+    SqlSqlTokenType,
+    TupleListSqlTokenType,
+    TupleSqlTokenType,
+    UnnestSqlTokenType,
+    ValueListSqlTokenType,
+} from "slonik";
+
+export type SlonikToken =
+    | IdentifierTokenType
+    | RawSqlTokenType
+    | SqlSqlTokenType
+    | TupleListSqlTokenType
+    | TupleSqlTokenType
+    | UnnestSqlTokenType
+    | ValueListSqlTokenType;
+
+export const isSlonikToken = (value: any): value is SlonikToken => {
+    return value && value.type;
+};
--- src/sqlx.ts
+++ src/sqlx.ts
@@ -1,9 +1,10 @@
-import { SqlSqlTokenType, sql } from "slonik";
+import { SqlSqlTokenType, ValueExpressionType, sql } from "slonik";
 import { processValue } from "./processValue";
 import { Sqlx } from "./SqlxClass";
 import { escapeString } from "./escapeString";
 import { wrapString } from "./wrapString";
 import { ProcessedValue } from "./ProcessedValue";
+import { isSlonikToken } from "./SlonikToken";
 
 export type SqlxTemplateFunc = (template: TemplateStringsArray, ...values: any[]) => SqlSqlTokenType;
 
@@ -21,15 +22,21 @@ const intoRaw = (processedValue: ProcessedValue) => {
     return sql.raw(value);
 };
 
+const handleValue = (value: any): ValueExpressionType => {
+    if (isSlonikToken(value)) {
+        return value;
+    }
+
+    return intoRaw(processValue(value, Sqlx._processors));
+};
+
 const internalSqlx = (options: SqlxOptions, template: TemplateStringsArray, ...values: any[]): SqlSqlTokenType => {
     if (options.prepared !== false) {
         return sql(template, ...values);
     }
 
-    const processedValues = values.map(value => processValue(value, Sqlx._processors));
-    const rawValues = processedValues.map(value => intoRaw(value));
-
-    return sql(template, ...rawValues);
+    const handledValues = values.map(value => handleValue(value));
+    return sql(template, ...handledValues);
 };
 
 const _sqlx: SqlxFunc = (template: TemplateStringsArray, ...values: any[]) => {

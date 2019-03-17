8f38219257ffcdcab3533bfeb93693f046e2439f
Radoslaw Medryk <radoslaw.medryk@yhoo.com>
2019-03-17 17:56:55 +0100

    Fix isSlonikToken

--- dist/types/SlonikToken.js
+++ dist/types/SlonikToken.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isSlonikToken = (value) => {
     // TODO [RM]: hacky
-    return value && value.type instanceof Symbol && value.type.toString().startsWith("Symbol(SLONIK");
+    return value && typeof value.type === "symbol" && value.type.toString().startsWith("Symbol(SLONIK");
 };
 //# sourceMappingURL=SlonikToken.js.map
